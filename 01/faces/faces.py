dac7517c3b2ad34146e492489838b8be9c5e31e8
Justin <koremis@proton.me>
2024-10-24 22:23:00 -0400

    Changes to be committed: 	new file:   faces.py

---
+++ 01/faces/faces.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""convert ascii emoji to emoji"""
+
+face = input("Make a face: ")
+if ":)" in face:
+    print(face.replace(":)", "😐"))
+if ":(" in face:
+    print(face.replace(":(", "🙁"))
+else:
+    print("I don't like that face")
