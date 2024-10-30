472a421481bbc7f39fca6515ac7785cee605cade
Justin <koremis@proton.me>
2024-10-29 20:08:15 -0400

    new file:   deep.py

---
+++ 02/deep/deep.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""The answer to everything"""
+
+def main():
+    answer = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ")
+    if answer == "42" or answer == "forty two" or answer == "forty-two" or answer == "Forty Two" or answer == "Forty-Two":
+        print("Yes")
+    else:
+        print("No")
+
+
+main()
